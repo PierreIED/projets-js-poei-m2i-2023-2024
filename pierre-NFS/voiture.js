8de4c013d94c97c9615cb87a0e23443ab9721083
Pierre Daheron <pierre.daheron@gmail.com>
2024-01-12 21:42:38 +0100

    code optimized and factorized

--- pierre-NFS/voiture.js
+++ pierre-NFS/voiture.js
@@ -1,56 +1,23 @@
-// Construction de la grille 
-var emptyLine = []
-for (let i = 0; i < 9; i++) emptyLine.push(0)
-var fullLine = [0, 1, 1, 1, 0, 1, 1, 1, 0]
-var myGrid = [emptyLine, fullLine, fullLine, fullLine, emptyLine, fullLine, fullLine, fullLine, emptyLine]
-var xCar = 1
-var yCar = 1
+var coordCar = [1, 1]
 
-function testMoveCar(){
-    if (xCar < 1 ||
-        xCar > 9 ||
-        yCar < 1 ||
-        yCar > 9 ||
-        myGrid[yCar - 1][xCar - 1] == 1)
-        {
-            return false
-        }
-    moveCar()
-    return true
+const available = (coord) => ([1, 5, 9].find(elt => elt == coord) != undefined)
+
+function testMoveCar(axe, op) {
+    if (!available(coordCar[(axe + 1) % 2]))
+        return
+    coordCar[axe] += eval(op + "1")
+    if (coordCar[axe] < 1 || coordCar[axe] > 9) {
+        coordCar[axe] -= eval(op + "1")
+    }
+    else {
+        moveCar()
+    }
 }
 
 function moveCar() {
     let myCar = document.getElementById('voiture')
-    myCar.style.gridColumnStart = xCar
-    myCar.style.gridColumnEnd = xCar + 1
-    myCar.style.gridRowStart = yCar
-    myCar.style.gridRowEnd = yCar + 1
-}
-
-function moveLeft() {
-    xCar--;
-    if (!testMoveCar()){
-        xCar++
-    }  
-}
-
-function moveRight() {
-    xCar++;
-    if (!testMoveCar()){
-        xCar--
-    }  
-}
-
-function moveTop() {
-    yCar--;
-    if (!testMoveCar()){
-        yCar++
-    }  
-}
-
-function moveBottom() {
-    yCar++;
-    if (!testMoveCar()){
-        yCar--
-    }  
+    myCar.style.gridColumnStart = coordCar[0]
+    myCar.style.gridColumnEnd = coordCar[0] + 1
+    myCar.style.gridRowStart = coordCar[1]
+    myCar.style.gridRowEnd = coordCar[1] + 1
 }
