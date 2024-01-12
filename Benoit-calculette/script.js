7296a9d109c720aef6f7daae12b57be5b44f4f1a
Benoit00 <benoitwitz0@gmail.com>
2024-01-12 16:56:13 +0100

    Simplification du code et possibilité d'entrer des chiffre négatifs et entrer plusieurs valeurs

--- Benoit-calculette/script.js
+++ Benoit-calculette/script.js
@@ -1,37 +1,37 @@
-var btns = document.querySelectorAll('button')
 var resultat = document.querySelector("#resultat")
-
 var valeurs = []
 var somme = 0
-var op
-var val1 = 0
-var val2 = 0
+
+// function ajouterValeur(chiffre){
+//     valeurs.push(chiffre)
+//     resultat.value = valeurs.join('')
+//     if(chiffre == '+' || chiffre == '-' || chiffre == '*' || chiffre == '/'){
+//         op = valeurs[valeurs.length-1]
+//         valeurs.pop()
+//         val1 = valeurs.join('')
+//         valeurs = []
+//     }else if(chiffre == '='){
+//         valeurs.pop()
+//         val2 = valeurs.join('')
+//         somme = eval(val1 + op + val2)
+//         valeurs = []
+//         resultat.value = somme
+//     }
+//     console.log(val1, op, val2);
+//     console.log(valeurs);
+// }
 
 function ajouterValeur(chiffre){
     valeurs.push(chiffre)
     resultat.value = valeurs.join('')
-    if(chiffre == '+' || chiffre == '-' || chiffre == '*' || chiffre == '/'){
-        op = valeurs[valeurs.length-1]
+    if(chiffre == '='){
         valeurs.pop()
-        val1 = valeurs.join('')
-        valeurs = []
-    }else if(chiffre == '='){
-        valeurs.pop()
-        val2 = valeurs.join('')
-        somme = eval(val1 + op + val2)
-        valeurs = []
+        somme = eval(valeurs.join(''))
         resultat.value = somme
     }
-    console.log(val1, op, val2);
-    console.log(valeurs);
 }
 
 function ClearResultat(){
-    val1 = 0
-    val2 = 0
-    op = 0
     resultat.value = ''
     valeurs = []
-    console.log(val1, op, val2);
-    console.log(valeurs);
 }
